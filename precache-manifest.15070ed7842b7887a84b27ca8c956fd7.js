b98e2c9dd1193022f09c42a225b616df8af1aa71
DanBrown501 <62338644+DanBrown501@users.noreply.github.com>
2022-05-16 22:32:29 -0500

    Updates

---
+++ precache-manifest.15070ed7842b7887a84b27ca8c956fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "570883a982441c61811d",
+    "url": "/static/css/main.8b93a9eb.chunk.css"
+  },
+  {
+    "revision": "570883a982441c61811d",
+    "url": "/static/js/main.4eb261c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bee763278f4d9ea48f7d",
+    "url": "/static/css/2.74a608af.chunk.css"
+  },
+  {
+    "revision": "bee763278f4d9ea48f7d",
+    "url": "/static/js/2.acac6e56.chunk.js"
+  },
+  {
+    "revision": "35bad3b960de94470324c220cce3941d",
+    "url": "/index.html"
+  }
+];
